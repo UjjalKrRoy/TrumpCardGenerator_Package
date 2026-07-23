b1597c7f277d20c2d6a7d628409a1ee8dc41369a
UjjalRoy <ujjalroy.indian@gmail.com>
2026-07-23 20:00:24 +0530

    Version 1.0.26

--- src/gui.py
+++ src/gui.py
@@ -98,7 +98,7 @@ class CardGeneratorGUI:
 
         base_dir = Path(__file__).resolve().parent.parent
 
-        self.root.title("Trump Card Generator v1.0.25")
+        self.root.title("Trump Card Generator v1.0.26")
 
         icon_path = base_dir / "icon.ico"
         if icon_path.exists():
